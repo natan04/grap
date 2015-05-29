9835ee47da629db6b2d0b3ce3f1a18cb4f2d1121
Natan <natan04@gmail.com>
2015-05-29 23:49:02 +0300

    finish?

--- Ass4/Ass4/main.cpp
+++ Ass4/Ass4/main.cpp
@@ -7,12 +7,18 @@
 #include <Windows.h>
 #include <fstream>
 #include "data.h"
-
+#define stringify( name ) # name
 #define MAX_CHARS_PER_LINE 500
 
-GLfloat modelMatrix[16];
+enum MODE {CAMERA, GLOBAL };
 
-GLfloat cameraMatrix[16];
+MODE sceneMode;
+
+
+GLfloat modelMatrix[16];
+GLfloat transCamera[16];
+GLdouble rotXcam, rotYcam;
+GLdouble scale = 1;
 
 int mouseState;
 GLfloat angle = 60;
@@ -26,9 +32,9 @@ float w, h, tip = 0, turn = 0;
 
 float ORG[3] = {0,0,0};
 
-float XP[3] = {00,0,0}, XN[3] = {-10,0,0},
-	YP[3] = {0,10,0}, YN[3] = {0,-10,0},
-	ZP[3] = {0,0,10}, ZN[3] = {0,0,-10};
+float XP[3] = {00,0,0}, XN[3] = {-100,0,0},
+	YP[3] = {0,0,0}, YN[3] = {0,-100,0},
+	ZP[3] = {0,0,0}, ZN[3] = {0,0,-100};
 
 using std::ifstream;
 using namespace std;
@@ -38,11 +44,17 @@ Data* data ;
 std::vector<ReturnedFace*> values;
 
 
+
+
 void disp()
 {
 		glMatrixMode(GL_MODELVIEW);
 		glLoadMatrixf(modelMatrix);
-		glMultMatrixf(cameraMatrix);
+		glRotated(rotXcam, 1, 0 ,0);
+		glRotated(rotYcam, 0, 1 ,0);
+		glMultMatrixf(transCamera);
+		glScaled(scale, scale, scale);
+		
 		glutPostRedisplay();
 }
 
@@ -54,13 +66,13 @@ void initLight()
 	GLfloat light_ambient[] = {0.5, 0.5, 0.5, 0.0}; //color
 	GLfloat light_diffuse[] = {0.0, 0.5, 0.5, 0.0}; //color
 	GLfloat light_specular[] = {0.0, 0.0, 0.5, 0.0}; 
-	//GLfloat light_position[]={-0.0,-1.5,1,0,0.0};
+	GLfloat light_position[]={-0.0,-1.5,1,0,0.0};
 	//GLfloat angle[] = {20.0};
 
 	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-	//glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
 	glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light_direction);
 	//glLightfv(GL_LIGHT0, GL_SPOT_CUTOFF,angle);
@@ -70,10 +82,10 @@ void initLight()
 	glEnable(GL_LIGHT0);
 	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER,GL_TRUE);
 	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE,GL_TRUE);
-	GLfloat mat_a[] = {0.1, 0.5, 0.5, 1.0};
-	GLfloat mat_d[] = {0.1, 0.7, 0.7, 1.0};
-	GLfloat mat_s[] = {1.0, 0.0, 0.9, 1.0};
-	GLfloat low_sh[] = {5.0};
+	GLfloat mat_a[] = {0.3,0.4,0.5,1};
+	GLfloat mat_d[] = {0.0,0.6,0.7,1};
+	GLfloat mat_s[] = {0.0,0.0,0.8,1};
+	GLfloat low_sh[] = {5.0};
 
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_a);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_d);
@@ -107,26 +119,24 @@ void readFromFile(char* value, Data* data)
 void Draw_Axes (void)
 {
 	glPushMatrix ();
-
-	glTranslatef (-2.4, -1.5, -5);
-	glRotatef (tip , 1,0,0);
-	glRotatef (turn, 0,1,0);
-	glScalef (0.25, 0.25, 0.25);
-
-	glLineWidth (2.0);
+	 glPushAttrib(GL_CURRENT_BIT);
+	 glMatrixMode(GL_MODELVIEW);
 
 	glBegin (GL_LINES);
 	glColor3f (1,0,0); // X axis is red.
-	glVertex3fv (ORG);
-	glVertex3fv (XP ); 
+	glVertex3f (0,0,0);
+	glVertex3f (100,0,0); 
+	glBegin (GL_LINES);
 	glColor3f (0,0,1); // Y axis is green.
-	glVertex3fv (ORG);
-	glVertex3fv (YP );
+	glVertex3f (0,0,0);
+	glVertex3f (0,100,0);
+	glBegin (GL_LINES);
 	glColor3f (0,1,0); // z axis is blue.
-	glVertex3fv (ORG);
-	glVertex3fv (ZP ); 
+	glVertex3f (0,0,0);
+	glVertex3f (0,0,-100);
 	glEnd();
 
+	glPopAttrib();
 	glPopMatrix ();
 }
 
@@ -140,22 +150,13 @@ void spaciel(int key, int x, int y)
 	switch(key)
 	{
 	case GLUT_KEY_UP : 
-		glPushMatrix();		
-		glLoadMatrixf(modelMatrix);
-		glScalef( 1 + 0.05 ,1+ 0.05 ,1.0f + 0.05 );
-		glGetFloatv(GL_MODELVIEW_MATRIX, modelMatrix);
-		glPopMatrix();
+		scale += 0.05;
 		disp();
 		break;
 
 	case  GLUT_KEY_DOWN : 
-		glPushMatrix();		
-		glLoadMatrixf(modelMatrix);
-		glScalef( 1 - 0.05 ,1 - 0.05 ,1.0f - 0.05 );
-		glGetFloatv(GL_MODELVIEW_MATRIX, modelMatrix);
-		glPopMatrix();
+		scale -= 0.05;
 		disp();
-		glutPostRedisplay();
 		break;
 
 	case GLUT_KEY_F2:
@@ -171,7 +172,7 @@ void spaciel(int key, int x, int y)
 
 	case GLUT_KEY_F3:
 		glMatrixMode(GL_PROJECTION); /* switch matrix mode */
-		
+		glLoadIdentity();
 		gluPerspective(angle -= 0.5 ,1,2,200);
 
 		disp();
@@ -190,35 +191,17 @@ void Keyboard (unsigned char key, int x, int y)
 	switch (key) {
 
 	case ' ' :  
-		glMatrixMode(GL_MODELVIEW);
-		glTranslatef(0,10,0);
-		glutPostRedisplay();
-		break;
+		if (sceneMode == CAMERA)
+			sceneMode = GLOBAL;
+		else
+			sceneMode = CAMERA;
 
-
-
-	default: printf ("   Keyboard %c == %d\n", key, key);
+		cout << "The current mode:" << sceneMode << endl;;
 	}
 
 	glutPostRedisplay();
 }
-void Draw_Teapot(void)
-{
-	glPushMatrix();
 
-	glTranslatef(0, 0, -5);
-	glRotatef(tip, 1, 0, 0);
-	glRotatef(turn, 0, 1, 0);
-
-	glColor3f(1.0, 0.5, 0.1);
-	glutSolidTeapot(1.0);
-
-	glLineWidth(2.0);
-	glColor3f(0.0, 0.2, 0.9);
-	glutWireTeapot(1.01);
-
-	glPopMatrix();
-}
 
 void mouse(int button, int state, int x, int y) 
 {
@@ -233,6 +216,7 @@ void display()
 
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
+	glDisable(GL_NORMALIZE);
 	for (std::vector<ReturnedFace*>::iterator it = values.begin(); it != values.end(); ++it){
 
 		GLuint runner;
@@ -256,6 +240,7 @@ void display()
 
 
 	}
+		Draw_Axes();
 
 	glFlush();
 
@@ -274,13 +259,10 @@ void init()
 	//defines view mode
 	gluPerspective(60,1,2,200);
 	
-	//glTranslatef(0,0,-100);
-	
-	//gluLookAt(0,-1,-1,0,-2,-2,1,1,0);  //define view direction
-	//gluLookAt(0,-1,-1,0,0,0,1,1,0);  //define view direction
-
 
 	glEnable(GL_DEPTH_TEST);  //define in which order the scene will built
+	glEnable(GL_NORMALIZE);
+
 	glDepthFunc(GL_LESS);
 	/* return to modelview mode */
 	glMatrixMode(GL_MODELVIEW);
@@ -288,12 +270,13 @@ void init()
 	
 	glPushMatrix();
 	glTranslatef(0,0,-100);
-	glGetFloatv(GL_MODELVIEW_MATRIX, cameraMatrix);
+	glGetFloatv(GL_MODELVIEW_MATRIX, transCamera);
 	glPopMatrix();
 //		
 	
 	glLoadIdentity();
 	glGetFloatv(GL_MODELVIEW_MATRIX, modelMatrix);
+
 
 }
 
@@ -301,65 +284,121 @@ void init()
 
 void motion(int x,int y)
 {
-	float sensitivity = 0.1;
-	int dy = -y + yy;
-	int dx = x - xx;
-	switch (mouseState){
+	GLint m_viewport[4];
+	glGetIntegerv( GL_VIEWPORT, m_viewport );
+	int dy = y - yy;
+	int dx = -x + xx;
+	xx = x;
+	yy = y;   
 
+switch (sceneMode){
+
+case  GLOBAL:
+			switch (mouseState){
+
+			case GLUT_LEFT_BUTTON:
+	
+				glMatrixMode(GL_MODELVIEW);
+
+				glPushMatrix();		
+				glLoadMatrixf(modelMatrix);
+				glRotatef( dy*180/m_viewport[3], 1 ,0,0 );
+				glRotatef( dx*180/m_viewport[2], 0 ,0,1 );
+		
+				glGetFloatv(GL_MODELVIEW_MATRIX, modelMatrix);
+				glPopMatrix();
+	
+				disp();
+				break;
+	
+
+
+			case GLUT_MIDDLE_BUTTON:	
+				
+				glMatrixMode(GL_MODELVIEW);
+				glPushMatrix();		
+				glLoadMatrixf(modelMatrix);
+				glTranslatef( 0 ,0,  dy*180/m_viewport[3] );
+	
+				glGetFloatv(GL_MODELVIEW_MATRIX, modelMatrix);
+				glPopMatrix();
+	
+				disp();
+				break;
+
+			case GLUT_RIGHT_BUTTON:	
+			
+				glMatrixMode(GL_MODELVIEW);
+
+				glPushMatrix();		
+				glLoadMatrixf(modelMatrix);
+				glTranslatef( 0,  dy*180/m_viewport[3], 0 );
+				glTranslatef( dx*180/m_viewport[2], 0,0 );
+
+		
+				glGetFloatv(GL_MODELVIEW_MATRIX, modelMatrix);
+				glPopMatrix();
+	
+				disp();
+				break;
+			}
+break;
+
+case CAMERA:
+	switch (mouseState){
+		
 	case GLUT_LEFT_BUTTON:
 	
+				glMatrixMode(GL_MODELVIEW);
 
-
-		glMatrixMode(GL_MODELVIEW);
-
-		glPushMatrix();		
-		glLoadMatrixf(modelMatrix);
-		glRotatef( dy*sensitivity, 1 ,0,0 );
-		glRotatef( dx*sensitivity, 0 ,0,1 );
+				rotXcam +=  dy*180/m_viewport[3];
+				rotYcam +=  dx*180/m_viewport[2];
 		
-		glGetFloatv(GL_MODELVIEW_MATRIX, modelMatrix);
-		glPopMatrix();
+				glPopMatrix();
 	
-		disp();
-		break;
+				disp();
+				break;
+
+	case GLUT_RIGHT_BUTTON:	
+			
+				glMatrixMode(GL_MODELVIEW);
+
+				glPushMatrix();		
+				glLoadMatrixf(transCamera);
+				glTranslatef( 0,  dy*50/m_viewport[3], 0 );
+				glTranslatef( dx*50/m_viewport[2], 0,0 );
+
+		
+				glGetFloatv(GL_MODELVIEW_MATRIX, transCamera);
+				glPopMatrix();
 	
+				disp();
+				break;
 
+		case GLUT_MIDDLE_BUTTON:	
+							
+			glMatrixMode(GL_MODELVIEW);
 
-	case GLUT_MIDDLE_BUTTON:	
+			glPushMatrix();		
+			glLoadMatrixf(transCamera);
+			glTranslatef( 0,0,  -dy*50/m_viewport[3] );
 				
 		
-		glMatrixMode(GL_MODELVIEW);
-
-		glPushMatrix();		
-		glLoadMatrixf(modelMatrix);
-		glTranslatef( 0 ,0,  dy*sensitivity );
-
-		
-		glGetFloatv(GL_MODELVIEW_MATRIX, modelMatrix);
-		glPopMatrix();
+			glGetFloatv(GL_MODELVIEW_MATRIX, transCamera);
+			glPopMatrix();
 	
-		disp();
-		break;
+				disp();
+				break;
 
-		case GLUT_RIGHT_BUTTON:	
-			
-		glMatrixMode(GL_MODELVIEW);
-
-		glPushMatrix();		
-		glLoadMatrixf(modelMatrix);
-		glTranslatef( 0,  dy*sensitivity, 0 );
-		glTranslatef( dx*sensitivity, 0,0 );
-
-		
-		glGetFloatv(GL_MODELVIEW_MATRIX, modelMatrix);
-		glPopMatrix();
-	
-		disp();
-		break;
 	}
-		xx = x;
-		yy = y;   
 
+			
+
+
+break;
+
+}
+	
 }
 
 int main(int  argc,  char** argv) 
@@ -368,7 +407,6 @@ int main(int  argc,  char** argv)
 	if  (argc >=  2)
 		readFromFile(argv[1], data);
 	else
-
 		exit(1);
 
 	values = data->paint();
